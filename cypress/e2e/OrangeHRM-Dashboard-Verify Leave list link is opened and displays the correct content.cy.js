d1442fd25e6b401c1e4d2ecbb5994eaad5a5ce43
Sheilasc <sheila.salinas@gmail.com>
2023-01-30 22:32:07 -0400

    Update test case

--- cypress/e2e/OrangeHRM-Dashboard-Verify Leave list link is opened and displays the correct content.cy.js
+++ cypress/e2e/OrangeHRM-Dashboard-Verify Leave list link is opened and displays the correct content.cy.js
@@ -15,9 +15,9 @@ describe('Test that Leave list link is opened and displays the correct content',
     cy.get ('[class="oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module"]').should('be.visible')
 
     //get Leave list option and click over it
-    //cy.get(':nth-child(2) > .oxd-icon-button > .oxd-icon>g>.cls-2').click()   // this is not working
+    cy.get('.oxd-grid-3 > :nth-child(2) > .oxd-icon-button > .oxd-icon').click() 
 
     //Verify Leave List section is displayed in the Leave page
-    //cy.get ('[class="oxd-text oxd-text--h5 oxd-table-filter-title"]').should('be.visible')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text--h6').contains('Leave').should('be.visible')
   })
 })
